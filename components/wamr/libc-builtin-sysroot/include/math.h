77e9dfc56b87ab8963695d7ed31badc6f1a8bd42
liuhan <liuhan@espressif.com>
2022-08-18 15:33:33 +0800

    feat(lvgl): Add more LVGL WASM wrapper API

---
+++ components/wamr/libc-builtin-sysroot/include/math.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#ifndef _WAMR_LIBC_MATH_H
+#define _WAMR_LIBC_MATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float sinf(float value);
+float cosf(float value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
